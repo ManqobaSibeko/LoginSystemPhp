3dd9772357ccb673883a15be7ae0feac518e060a
manqsibeko1@gmail.com <manqsibeko1@gmail.com>
2020-09-08 14:57:37 +0200

    include folder

--- header.php
+++ header.php
@@ -7,5 +7,18 @@
 </head>
 <body>
     <header>
+        <nav>
+        <a href = "">Logo</a>
+            <ul>
+                <li><a href = "index.php">home</a></li>
+                <li><a href = "">Potfolio</a></li>
+                <li><a href = "">About Me</a></li>
+                <li><a href = "">Contact</a></li>
+            </ul>
+            <div>
+            //include the login form and logout from 
+            <form action= "includes/login.inc.php" method="post">
+            </div>
+        </nav>
 
-    <header>
+    </header>
